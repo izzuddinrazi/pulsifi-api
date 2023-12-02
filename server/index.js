764f7e093f248b5f87c36d88114fc9f43804049f
izzuddin <izzuddin.razi@revmedia.my>
2023-12-02 21:07:08 +0800

    test ci

--- server/index.js
+++ server/index.js
@@ -6,7 +6,7 @@ const fastify = require('fastify')({
   
   // Declare a route
   fastify.get('/', function (request, reply) {
-    reply.send({ hello: 'Pulsifi Assignment 1' })
+    reply.send({ hello: 'Pulsifi Assignment 1 - create CI' })
   })
   
   // Run the server!
