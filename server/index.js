bffb2266e2f0c31d0567e53b657d4e5b7722dc62
izzuddin <izzuddin.razi@revmedia.my>
2023-12-02 19:26:26 +0800

    add fastify and docker dev

---
+++ server/index.js
@@ -0,0 +1,19 @@
+// index.js
+
+const fastify = require('fastify')({
+    logger: true
+  })
+  
+  // Declare a route
+  fastify.get('/', function (request, reply) {
+    reply.send({ hello: 'Pulsifi Assignment 1' })
+  })
+  
+  // Run the server!
+  fastify.listen({ port: 3000, host: '0.0.0.0' }, function (err, address) {
+    if (err) {
+      fastify.log.error(err)
+      process.exit(1)
+    }
+    // Server is now listening on ${address}
+  })
